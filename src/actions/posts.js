11015628e56c223bf06f5f3f14ca184289cf87e7
anirbank-89 <86946524+anirbank-89@users.noreply.github.com>
2021-09-24 15:06:07 +0530

    third commit

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -5,11 +5,23 @@ export const getPosts = ()=>async (dispatch)=>{
     try {
         const { data } = await api.fetchPosts();
 
-        const action = {type: 'FETCH_ALL', payload: data }
+        const action = {type: 'FETCH_ALL', payload: data}
         
         dispatch(action);
     }
     catch (err) {
         console.log(err.message);
+    }
+}
+
+export const createPost = (post)=>async (dispatch)=>{
+    try {
+        const { data } = api.createPost(post);
+
+        const action = {type: 'CREATE', payload: data}
+
+        dispatch(action);
+    } catch (err) {
+        console.log(err);
     }
 }
