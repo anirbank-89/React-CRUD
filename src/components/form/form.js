43afcc1eb3a77e8231c4d8208b9980f0d7475ab7
anirbank-89 <86946524+anirbank-89@users.noreply.github.com>
2021-09-26 01:00:33 +0530

    api update request

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import useStyles from './styles';
-import { createPost } from '../../actions/posts';
+import { createPost, updatePost } from '../../actions/posts';
 
-const Form = ()=>{
+// GET THE CURRENT ID
+
+const Form = ({ currentId, setCurrentId})=>{
     const [postData, setPostData] = useState({
         creator: '',
         title: '',
@@ -14,20 +16,40 @@ const Form = ()=>{
         tags: '',
         selectedFile: ''
     });
+    const post = useSelector((state)=>currentId ? state.posts.find((p)=>p._id===currentId) : null);
     const classes = useStyles();
     const dispatch = useDispatch();
+
+    useEffect(()=>{
+        if(post) setPostData(post);
+    }, [post])
 
     const handleSubmit = (e)=>{
         e.preventDefault();
 
-        dispatch(createPost(postData));
+        if (currentId!=null) {
+            dispatch(updatePost(currentId, postData));
+        }
+        else {
+            dispatch(createPost(postData));
+        }
+        clear();
     }
 
-    const clear = ()=>{}
+    const clear = ()=>{
+        setCurrentId(null);
+        setPostData({
+            creator: '',
+            title: '',
+            message: '',
+            tags: '',
+            selectedFile: ''
+        });
+    }
     return (
         <Paper className={classes.paper}>
             <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant='h6'>Creating a memory</Typography>
+                <Typography variant='h6'>{currentId ? 'Editing' : 'Creating'} a memory</Typography>
                 <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e)=>setPostData({ ...postData, creator: e.target.value })} />
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e)=>setPostData({ ...postData, title: e.target.value })} />
                 <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e)=>setPostData({ ...postData, message: e.target.value })} />
--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -6,14 +6,20 @@ import Post from './post/post';
 
 import useStyles from './styles';
 
-const Posts = ()=>{
+const Posts = ({ setCurrentId})=>{
     const posts = useSelector((state)=>state.posts);
     const classes = useStyles();
 
     console.log(posts);
     return (
         !posts.length ? <CircularProgress /> : (   // if posts.length is 0, the show CircularProgress
-            <Grid></Grid>                          // if posts.length is >0, the show the posts
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {posts.map(post=>(                 // if posts.length is >0, the show the posts
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} setCurrentId={setCurrentId} />
+                    </Grid>
+                ))}
+            </Grid>
         )
     );
 }
